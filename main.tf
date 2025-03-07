66c40f4e397dceabd943509532191c9f3daafe7a
danybruun <84915043+danybruun@users.noreply.github.com>
2025-03-07 09:46:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used test
+# Azure Provider source and version being used
 terraform {
   backend "azurerm" {
     resource_group_name  = "runner"
